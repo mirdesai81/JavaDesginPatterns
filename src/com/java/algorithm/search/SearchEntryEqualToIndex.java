e1a5a728c07052a3bb4a57decde017bc249bc804
Mihir Desai <mirdesai81@gmail.com>
2020-05-12 12:01:34 -0700

    Find magic index equal to array entry

--- src/com/java/algorithm/search/SearchEntryEqualToIndex.java
+++ src/com/java/algorithm/search/SearchEntryEqualToIndex.java
@@ -27,8 +27,38 @@ public class SearchEntryEqualToIndex {
         return -1;
     }
 
+    public static int searchMagicNumber(List<Integer> list) {
+        return searchMagicNumber(list,0,list.size() - 1);
+    }
+
+    private static int searchMagicNumber(List<Integer> list,int start,int end) {
+        if(end < start) return -1;
+
+        int midIndex = start + ((end - start) / 2);
+        int midValue = list.get(midIndex);
+
+        if(midIndex == midValue) return midIndex;
+
+        int leftIndex = Math.min(midIndex - 1,midValue);
+        int left = searchMagicNumber(list,start,leftIndex);
+        if(left >= 0) return left;
+
+        int rightIndex = Math.max(midIndex + 1, midValue);
+        int right = searchMagicNumber(list,rightIndex,end);
+
+        return right;
+    }
+
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(-2, 0,2,3,6,7,9));
+
+        /*
+            0      1  2  3  4 5 6 7 8 9 10
+            -40 ,-20, 2, 2, 3 4 5 7
+         */
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(-40 ,-20, 2, 2, 3, 4, 5, 7,7,9,10));
         StdOut.println(searchEntryEqualToIndex(list));
+
+        StdOut.println(searchMagicNumber(list));
     }
 }
