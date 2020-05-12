391d45c81cd2231fbb366a0a4abafee2276d807a
Mihir Desai <mirdesai81@gmail.com>
2020-05-12 12:00:46 -0700

    Find all subsets

--- src/com/java/algorithm/array/Subsets.java
+++ src/com/java/algorithm/array/Subsets.java
@@ -1,5 +1,10 @@
 package com.java.algorithm.array;
 
+import com.java.stdlib.StdOut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by mihir.desai on 4/13/2018.
  */
@@ -18,10 +23,37 @@ public class Subsets {
         }
     }
 
+    public static ArrayList<ArrayList<Character>> getSubsets(ArrayList<Character> set,int index) {
+        ArrayList<ArrayList<Character>> allSubsets;
+        if(set.size() == index) {
+            allSubsets = new ArrayList<>();
+            allSubsets.add(new ArrayList<Character>());
+        } else {
+            allSubsets = getSubsets(set,index + 1);
+            Character item = set.get(index);
+            ArrayList<ArrayList<Character>> moreSubsets = new ArrayList<>();
+            for(ArrayList<Character> currSubset : allSubsets) {
+                ArrayList<Character> newSubset = new ArrayList<>();
+                newSubset.addAll(currSubset);
+                newSubset.add(item);
+                moreSubsets.add(newSubset);
+            }
+
+            allSubsets.addAll(moreSubsets);
+        }
+
+        return allSubsets;
+    }
+
     // Driver code
     public static void main(String[] args)
     {
         char set[] = {'a', 'b', 'c'};
         printSubsets(set);
+        StdOut.println();
+        ArrayList<Character> list = new ArrayList<>(Arrays.asList('a','b','c'));
+        StdOut.println(getSubsets(list,0));
+
+
     }
 }
