f4aaf49c68755804a432a112afd6b386a1372541
Mihir Desai <mirdesai81@gmail.com>
2020-05-13 17:04:51 -0700

    Generate Parenthesis

--- src/com/java/algorithm/string/GenerateParens.java
+++ src/com/java/algorithm/string/GenerateParens.java
@@ -2,6 +2,7 @@ package com.java.algorithm.string;
 
 import com.java.stdlib.StdOut;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -34,7 +35,29 @@ public class GenerateParens {
         return start + "()" + end;
     }
 
+    public static ArrayList<String> generateParenEfficient(int count) {
+        char[] str = new char[count*2];
+        ArrayList<String> result = new ArrayList<>();
+        addParen(result,count,count,str,0);
+        return result;
+    }
+
+    private static void addParen(ArrayList<String> list,int leftRem,int rightRem,char[] str,int index) {
+        if(leftRem < 0 || rightRem < leftRem) return;
+
+        if(leftRem == 0 && rightRem == 0) {
+            list.add(String.copyValueOf(str));
+        } else {
+            str[index] = '(';
+            addParen(list,leftRem - 1,rightRem,str,index + 1);
+
+            str[index] = ')';
+            addParen(list,leftRem,rightRem - 1,str,index + 1);
+        }
+    }
+
     public static void main(String[] args) {
         StdOut.println(generateParen(3));
+        StdOut.println(generateParenEfficient(3));
     }
 }
