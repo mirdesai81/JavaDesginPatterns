a16a2b1ba271ad92a4f02719ae43bea4a72d4c24
Mihir Desai <mirdesai81@gmail.com>
2020-05-22 14:56:09 -0700

    Move even at the beginning of the array

--- src/com/java/algorithm/array/EvenOdd.java
+++ src/com/java/algorithm/array/EvenOdd.java
@@ -17,12 +17,36 @@ public class EvenOdd {
         }
     }
 
+    public static void eventAtEnd(int[] a) {
+        int low = 0, high = a.length - 1;
+
+        while(low < high) {
+            if(a[low] % 2 != 0) {
+                low++;
+            }
+            if(a[high] % 2 == 0) {
+                high--;
+            }
+
+            swap(a,low,high);
+            low++;
+            high--;
+        }
+    }
+
+    public static void swap(int[] a,int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
 
 
     public static void main(String[] args) {
         int[] a = {10,13,14,15,16,12,11,17,18,19,20};
         ArrayUtils.print("Before",a);
-        evenOdd(a);
+//        evenOdd(a);
+        eventAtEnd(a);
         ArrayUtils.print("After",a);
     }
 }
