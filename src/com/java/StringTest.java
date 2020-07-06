dae4e21a5527a55df30548cd92bc4040ea493eb2
Mihir Desai <mirdesai81@gmail.com>
2020-07-06 10:02:24 -0700

    String base 64

--- src/com/java/StringTest.java
+++ src/com/java/StringTest.java
@@ -1,6 +1,65 @@
 package com.java;
 
+import java.math.BigInteger;
+
 public class StringTest {
+
+
+    private static final char[] corpus   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_".toCharArray();
+    /*
+     * Note if seed is unique then generated base62 number will be unique as well under load balance make sure this value is not same.
+     */
+    public static final String getBase64From10(final long seed)
+    {
+        String number = seed + "";
+        char[] buf = new char[number.length()];
+        int charPos = number.length() - 1;
+        BigInteger bigIntegerNumber = new BigInteger(number);
+        BigInteger radix = BigInteger.valueOf(64);
+
+        while (bigIntegerNumber.compareTo(radix) >= 0)
+        {
+            buf[charPos--] = corpus[bigIntegerNumber.mod(radix).intValue()];
+            bigIntegerNumber = bigIntegerNumber.divide(radix);
+        }
+        buf[charPos] = corpus[bigIntegerNumber.intValue()];
+        return new String(buf, charPos, (number.length() - charPos));
+    }
+    /**
+     * @param longNumber
+     * a positive number in base 64
+     * @return the same number, in base 10
+     */
+    public static final String getBase10From64(final String number)
+    {
+
+        BigInteger value = BigInteger.ZERO;
+        for (char c : number.toCharArray())
+        {
+            value = value.multiply(BigInteger.valueOf(64));
+            if ('0' <= c && c <= '9')
+            {
+                value = value.add(BigInteger.valueOf(c - '0'));
+            }
+            if ('a' <= c && c <= 'z')
+            {
+                value = value.add(BigInteger.valueOf(c - 'a' + 10));
+            }
+            if ('A' <= c && c <= 'Z')
+            {
+                value = value.add(BigInteger.valueOf(c - 'A' + 36));
+            }
+
+            //45  -
+            //97 _
+            if('-' == c || '_' == c) {
+                value = value.add(BigInteger.valueOf(Character.getNumericValue(c)));
+            }
+
+        }
+        return value.toString();
+    }
+
 
     public static void main(String[] args) {
 
@@ -27,6 +86,13 @@ public class StringTest {
 
         String s5 = "CFG";
         System.out.println("s4 == s5 "+(s4 == s5));
+
+
+        String base64String = getBase64From10(1634000000);
+        System.out.println(base64String);
+
+        String base10String = getBase10From64("bHznYa");
+        System.out.println(base10String);
 
     }
 }
